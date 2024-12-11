f8fd67a3883f4df3c65cb135b388661981e50356
wlc952 <wlc@zju.edu.cn>
2024-12-11 11:37:20 +0800

    update download.sh

--- download.sh
+++ download.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-wget https://modelscope.cn/models/wlc952/aigchub_models/resolve/master/rmbg/rmbg-1.4_1024.bmodel -O hivision/creator/weights/rmbg-1.4.bmodel
+wget https://modelscope.cn/models/wlc952/aigchub_models/resolve/master/rmbg/rmbg-1.4_1024.tar.gz
+tar zxvf rmbg-1.4_1024.tar.gz
+mv rmbg-1.4_1024.bmodel hivision/creator/weights/rmbg-1.4.bmodel
+rm rmbg-1.4_1024.tar.gz
 
-wget https://modelscope.cn/models/wlc952/aigchub_models/resolve/master/retinaface/retinaface-resnet50_512.bmodel -O hivision/creator/retinaface/weights/retinaface-resnet50.bmodel
-
+wget https://modelscope.cn/models/wlc952/aigchub_models/resolve/master/retinaface/retinaface-resnet50_512.tar.gz
+tar zxvf retinaface-resnet50_512.tar.gz
+mv retinaface-resnet50_512.bmodel hivision/creator/retinaface/weights/retinaface-resnet50.bmodel
+rm retinaface-resnet50_512.tar.gz
